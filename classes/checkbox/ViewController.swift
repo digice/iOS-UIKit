2dfd1bd521868fb018caa18fdadecf290bd12149
Roderic Linguri <linguri@digices.com>
2017-05-05 09:37:30 -0600

    Create ViewController.swift

---
+++ classes/checkbox/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  iOS-UIKit
+//  Checkbox Implementation Example
+//  Created by Roderic Linguri on 5/5/17.
+//  Copyright © 2017 Digices LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, CheckboxDelegate {
+
+  // MARK: - IBOUtlets
+  
+  @IBOutlet weak var myCheckbox: Checkbox!
+  
+  // MARK: - UIViewController
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+     self.myCheckbox.delegate = self
+  }
+
+  // MARK: - CheckboxDelegate
+
+  func checkboxWasChecked() {
+    print("checked!")
+  }
+  
+  func checkboxWasUnchecked() {
+    print("unchecked!")
+  }
+
+}
