d373b4e54a3591d0a47dfe96f1984d25089fa521
Roderic Linguri <linguri@digices.com>
2017-05-05 09:16:24 -0600

    Update Checkbox.swift

--- classes/Checkbox.swift
+++ classes/Checkbox.swift
@@ -22,6 +22,7 @@ class Checkbox: UIButton {
 
   override func awakeFromNib() {
     self.addTarget(self, action: #selector(self.toggle), for: .touchUpInside)
+    self.contentMode = .redraw
   }
 
   func toggle() {
@@ -34,12 +35,14 @@ class Checkbox: UIButton {
   }
 
   func box() -> UIBezierPath {
+    let offset = self.stroke / 2
     let path = UIBezierPath()
-    path.move(to: CGPoint(x: 0 + self.stroke, y: 0 + self.stroke))
-    path.addLine(to: CGPoint(x: self.bounds.width - self.stroke, y: 0 + self.stroke))
-    path.addLine(to: CGPoint(x: self.bounds.width - self.stroke, y: self.bounds.height - self.stroke))
-    path.addLine(to: CGPoint(x: 0 + self.stroke, y: self.bounds.height - self.stroke))
-    path.addLine(to: CGPoint(x: 0 + self.stroke, y: 0 + (self.stroke / 2)))
+    path.move(to: CGPoint(x: 0, y: 0 + offset))
+    path.addLine(to: CGPoint(x: self.bounds.width - offset, y: 0 + offset))
+    path.addLine(to: CGPoint(x: self.bounds.width - offset, y: self.bounds.height - offset))
+    path.addLine(to: CGPoint(x: 0 + offset, y: self.bounds.height - offset))
+    path.addLine(to: CGPoint(x: 0 + offset, y: 0))
+    path.lineWidth = self.stroke
     return path
   }
 
@@ -49,6 +52,7 @@ class Checkbox: UIButton {
     path.addLine(to: CGPoint(x: self.bounds.width - self.stroke, y: self.bounds.height - self.stroke))
     path.move(to: CGPoint(x: self.bounds.width - self.stroke, y: 0 + self.stroke))
     path.addLine(to: CGPoint(x: 0 + self.stroke, y: self.bounds.height - self.stroke))
+    path.lineWidth = self.stroke
     return path
   }
 
